599c58e3101126e5fda1ae4e90c704d4f10dce26
Titruc <86261407+Titruc@users.noreply.github.com>
2024-07-11 16:11:58 +0200

    multiplayer setup
    TODO : handle disconnection and connection and host disconnected

--- game/data/script/entity/player/default/controler.gd
+++ game/data/script/entity/player/default/controler.gd
@@ -12,64 +12,68 @@ var bob_progress : float = 0.0
 
 
 @onready var head = $head
-@onready var camera = $head/playerCamera
+@onready var camera : Camera3D = $head/playerCamera
 @export var velocityHandler : velocityComponent
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	set_multiplayer_authority(name.to_int())
+	camera.make_current()
+	print(name.to_int())
+	print("aaa", camera, name)
 	
 func _unhandled_input(event):
 	
 	#head turn
+	
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * playerAttribute.SENSITIVITY)
 		camera.rotate_x(-event.relative.y * playerAttribute.SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60),deg_to_rad(60))
+		
 
 func _physics_process(delta):
 	# Add the gravity.
-	velocityHandler.setCurrentVelocity(velocity)
-	
-	if isAbleToMove:
-		if not is_on_floor():
-			velocityHandler.applyGravity(playerAttribute.GRAVITY, delta)
+	if is_multiplayer_authority():
+		velocityHandler.setCurrentVelocity(velocity)
+		
+		if isAbleToMove:
+			if not is_on_floor():
+				velocityHandler.applyGravity(playerAttribute.GRAVITY, delta)
 
-		# Handle Jump.
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocityHandler.setVelocityY(playerAttribute.JUMP_VELOCITY)
+			# Handle Jump.
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocityHandler.setVelocityY(playerAttribute.JUMP_VELOCITY)
+				
+			#sprint and walk
 			
-		#sprint and walk
-		
-		#and speed and camera fov
-		if Input.is_action_pressed("sprint"):
-			speed = lerp(speed,playerAttribute.SPRINT_SPEED,playerAttribute.SPEED_VARIATION)
-			camera.fov = lerp(camera.fov, playerAttribute.FOV+(playerAttribute.FOV_VARIATION * clamp(velocity.length(),0,1)), 0.1)
+			#and speed and camera fov
+			if Input.is_action_pressed("sprint"):
+				speed = lerp(speed,playerAttribute.SPRINT_SPEED,playerAttribute.SPEED_VARIATION)
+				camera.fov = lerp(camera.fov, playerAttribute.FOV+(playerAttribute.FOV_VARIATION * clamp(velocity.length(),0,1)), 0.1)
+				
+			else:
+				speed = lerp(speed,playerAttribute.WALK_SPEED,playerAttribute.SPEED_VARIATION)
+				camera.fov = lerp(camera.fov, float(playerAttribute.FOV), 0.1)
+				
+			#handle direction
+			var input_dir = Input.get_vector("left", "right", "up", "down")
+			var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 			
-		else:
-			speed = lerp(speed,playerAttribute.WALK_SPEED,playerAttribute.SPEED_VARIATION)
-			camera.fov = lerp(camera.fov, float(playerAttribute.FOV), 0.1)
+			if direction:
+				velocityHandler.setVelocityXZ(Vector2(direction.x * speed,direction.z * speed))
+			else:
+				velocityHandler.setVelocityXZ(Vector2(0, 0))
 			
-		#handle direction
-		var input_dir = Input.get_vector("left", "right", "up", "down")
-		var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		
-		if direction:
-			velocityHandler.setVelocityXZ(Vector2(direction.x * speed,direction.z * speed))
-		else:
-			velocityHandler.setVelocityXZ(Vector2(0, 0))
-		
-		#head bobing
-		bob_progress += delta * velocity.length() * float(is_on_floor())
-		camera.transform.origin = _headbob(bob_progress)
-		
-		velocity = velocityHandler.getFinalVelocity()
-		move_and_slide()
+			#head bobing
+			bob_progress += delta * velocity.length() * float(is_on_floor())
+			camera.transform.origin = _headbob(bob_progress)
+			
+			velocity = velocityHandler.getFinalVelocity()
+	move_and_slide()
 	
 func _headbob(time) -> Vector3:
 	var pos : Vector3 = Vector3.ZERO
 	pos.y = sin(time * playerAttribute.BOB_FREQUENTY) * playerAttribute.BOB_AMPLITUDE
 	pos.x = cos(time * playerAttribute.BOB_FREQUENTY/2.0) * playerAttribute.BOB_AMPLITUDE
 	return pos
-
-func _on_area_3d_area_entered(area):
-	print(area)
---
+++ game/data/script/multiplayer/multiplayerControler.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var peer = ENetMultiplayerPeer.new()
+@export var playerScene : PackedScene
+@export var lobbyScene : Node3D
+@export var gameControler : Node
+@export var menu : Node2D
+
+
+func _on_host_pressed():
+	peer.create_server(1617, 6)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_addPlayer)
+	menu.hide()
+	_addPlayer()
+
+func _addPlayer(id = 1):
+	print(id)
+	var player = playerScene.instantiate()
+	player.name = str(id)
+	lobbyScene.call_deferred("add_child",player)
+	
+	
+func _on_join_pressed():
+	peer.create_client("localhost",1617)
+	multiplayer.multiplayer_peer = peer
+	menu.hide()
+	
