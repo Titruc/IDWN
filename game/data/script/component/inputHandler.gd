06a009142d6ba7c697519deac84ef989a52980b3
Titruc <86261407+Titruc@users.noreply.github.com>
2024-07-12 22:11:32 +0200

    fix because consol is full

--- game/data/script/component/inputHandler.gd
+++ game/data/script/component/inputHandler.gd
@@ -17,7 +17,8 @@ func _gather():
 		isSprinting = Input.is_action_pressed("sprint")
 		direction = Input.get_vector("left", "right", "up", "down")
 func _process(delta):
-	print("jump : " , Input.is_action_just_pressed("jump"))
+	if Input.is_action_just_pressed("jump"):
+		print("jump : " , Input.is_action_just_pressed("jump"))
 	if not is_multiplayer_authority():
 		return
 	if get_parent().name == "1":
