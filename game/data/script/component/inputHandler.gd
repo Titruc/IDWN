0f1adafcdd33206521921c548303b59f006c2f67
Titruc <86261407+Titruc@users.noreply.github.com>
2024-07-13 12:41:12 +0200

    jump just_pressed change to pressed

--- game/data/script/component/inputHandler.gd
+++ game/data/script/component/inputHandler.gd
@@ -11,23 +11,16 @@ func _ready():
 	NetworkTime.before_tick_loop.connect(_gather)
 
 func _gather():
-	if Input.is_action_just_pressed("jump"):
-		print("jump netwotk : " , Input.is_action_just_pressed("jump"))
+	
 	if not is_multiplayer_authority():
 		return
-	if get_parent().name != "1":
-		asJump = Input.is_action_just_pressed("jump")
-		isSprinting = Input.is_action_pressed("sprint")
-		direction = Input.get_vector("left", "right", "up", "down")
-		leftClick = Input.is_action_just_pressed("left_click")
+	
+	asJump = Input.is_action_pressed("jump")
+	isSprinting = Input.is_action_pressed("sprint")
+	direction = Input.get_vector("left", "right", "up", "down")
+	leftClick = Input.is_action_pressed("left_click")
+	print("has been gather : ", asJump)
+	
 func _process(delta):
-	if Input.is_action_just_pressed("jump"):
-		print("jump : " , Input.is_action_just_pressed("jump"), asJump)
-	if not is_multiplayer_authority():
-		return
-	if get_parent().name == "1":
-		asJump = Input.is_action_just_pressed("jump")
-		isSprinting = Input.is_action_pressed("sprint")
-		direction = Input.get_vector("left", "right", "up", "down")
-		leftClick = Input.is_action_just_pressed("left_click")
+	pass
 	
